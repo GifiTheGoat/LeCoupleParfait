e8ace81622db191bf17b57262e3ade8568af36dd
Gmenou <37302642+Gmenou@users.noreply.github.com>
2020-12-21 15:58:44 +0100

    Create upload.php

---
+++ upload.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport"    content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="">
+  <meta name="author"      content="Gifi & Titi">
+  
+  <title>Guillaume et Eugénie</title>
+
+  <link rel="shortcut icon" href="assets/images/E_G.png">
+  
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/css/all.css">
+
+  <!-- Custom styles for our template -->
+  <link rel="stylesheet" href="assets/css/bootstrap-theme.css" media="screen" >
+  <link rel="stylesheet" href="assets/css/main.css">
+
+  <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!--[if lt IE 9]>
+  <script src="assets/js/html5shiv.js"></script>
+  <script src="assets/js/respond.min.js"></script>
+  <![endif]-->
+</head>
+<body class="home" style = "background-color: ##d2d5d5">
+  <!-- Fixed navbar -->
+  <div class="navbar navbar-inverse navbar-fixed-top headroom" >
+    <div class="container">
+      <div class="navbar-header">
+        <a class="navbar-brand" href="index.html"><img src="assets/images/EGrose.png" height="40px" alt="Progressus HTML5 template"></a>
+      </div>
+      <div class="navbar-collapse collapse">
+        <ul class="nav navbar-nav pull-right">
+          <li class="active"><a>Merci pour votre message</a></li>
+        </ul>
+      </div>
+      
+    </div>
+  </div> 
+  <!-- /.navbar -->
+
+<?php
+ 
+/************************************************************
+ * Script realise par Emacs
+ * Crée le 19/12/2004
+ * Maj : 23/06/2008
+ * Licence GNU / GPL
+ * webmaster@apprendre-php.com
+ * http://www.apprendre-php.com
+ * http://www.hugohamon.com
+ *
+ * Changelog:
+ *
+ * 2008-06-24 : suppression d'une boucle foreach() inutile
+ * qui posait problème. Merci à Clément Robert pour ce bug.
+ *
+ *************************************************************/
+ 
+/************************************************************
+ * Definition des constantes / tableaux et variables
+ *************************************************************/
+ 
+// Constantes
+define('TARGET', '/home/pi/Documents/Photos_mariage');    // Repertoire cible
+//define('MAX_SIZE', 100000);    // Taille max en octets du fichier
+//define('WIDTH_MAX', 800);    // Largeur max de l'image en pixels
+//define('HEIGHT_MAX', 800);    // Hauteur max de l'image en pixels
+ 
+// Tableaux de donnees
+$tabExt = array('jpg','gif','png','jpeg');    // Extensions autorisees
+$infosImg = array();
+ 
+// Variables
+$extension = '';
+$message = '';
+$nomImage = '';
+ 
+/************************************************************
+ * Creation du repertoire cible si inexistant
+ *************************************************************/
+if( !is_dir(TARGET) ) {
+  if( !mkdir(TARGET, 0755) ) {
+    exit('Erreur : le répertoire cible ne peut-être créé ! Vérifiez que vous diposiez des droits suffisants pour le faire ou créez le manuellement !');
+  }
+}
+ 
+/************************************************************
+ * Script d'upload
+ *************************************************************/
+if(!empty($_POST))
+{
+  // On verifie si le champ est rempli
+  if( !empty($_FILES['fichier']['name']) )
+  {
+    // Recuperation de l'extension du fichier
+    $extension  = pathinfo($_FILES['fichier']['name'], PATHINFO_EXTENSION);
+ 
+    // On verifie l'extension du fichier
+    if(in_array(strtolower($extension),$tabExt))
+    {
+      // On recupere les dimensions du fichier
+      $infosImg = getimagesize($_FILES['fichier']['tmp_name']);
+ 
+      // On verifie le type de l'image
+      if($infosImg[2] >= 1 && $infosImg[2] <= 14)
+      {
+        // On verifie les dimensions et taille de l'image
+//        if(($infosImg[0] <= WIDTH_MAX) && ($infosImg[1] <= HEIGHT_MAX) && (filesize($_FILES['fichier']['tmp_name']) <= MAX_SIZE))
+//        {
+          // Parcours du tableau d'erreurs
+          if(isset($_FILES['fichier']['error']) 
+            && UPLOAD_ERR_OK === $_FILES['fichier']['error'])
+          {
+            // On renomme le fichier
+            $nomImage = md5(uniqid()) .'.'. $extension;
+ 
+            // Si c'est OK, on teste l'upload
+            if(move_uploaded_file($_FILES['fichier']['tmp_name'], TARGET.$nomImage))
+            {
+              $message = 'Upload réussi !';
+            }
+            else
+            {
+              // Sinon on affiche une erreur systeme
+              $message = 'Problème lors de l\'upload !';
+            }
+          }
+          else
+          {
+            $message = 'Une erreur interne a empêché l\'uplaod de l\'image';
+          }
+//        }
+//        else
+//        {
+//          // Sinon erreur sur les dimensions et taille de l'image
+//          $message = 'Erreur dans les dimensions de l\'image !';
+//        }
+      }
+      else
+      {
+        // Sinon erreur sur le type de l'image
+        $message = 'Le fichier à uploader n\'est pas une image !';
+      }
+    }
+    else
+    {
+      // Sinon on affiche une erreur pour l'extension
+      $message = 'L\'extension du fichier est incorrecte !';
+    }
+  }
+  else
+  {
+    // Sinon on affiche une erreur pour le champ vide
+    $message = 'Veuillez remplir le formulaire svp !';
+  }
+}
+?>
+
+
+  <div class="container text-center"><h2><br><br><br><br>Merci pour votre message, si vous attendez une réponse, nous vous l'enverrons au plus vite.
+  <br>En attendant on vous fait des bisous ;)<br><br></h2>
+  <p><a class="btn btn-default btn-lg" role="button" href="index.html">Retour à la page d'accueil</a></p></div>
+  <p style="color:white">USER : A l'issue de noirs combats<br>On l'a scellée maintes fois<br>Elle vous remplie d'émoi<br>lorsqu'on la reçoit (8 lettres, la première sera en majuscule)</p>
+
+  <script src="assets/js/headroom.min.js"></script>
+  <script src="assets/js/jQuery.headroom.min.js"></script>
+  <script src="assets/js/template.js"></script>
+    
+</body>
+</html>
+<?php
+}
+?>
